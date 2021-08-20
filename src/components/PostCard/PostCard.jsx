5f0141624dd7d14bd3fde3c5f2467ae2c3b3e9e6
Jai Arora <jai.arora@timesinternet.in>
2021-08-20 17:03:33 +0530

    ADD: Share Popover

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -5,22 +5,36 @@ import {
   LikeOutlined,
   ShareAltOutlined,
 } from "@ant-design/icons";
-import { Image } from "antd";
+import { Image, Popover } from "antd";
 import isUrl from "../../common/helpers/isUrl";
 import CommentBox from "../CommentBox/CommentBox";
 import "./PostCard.scss";
 import PostService from "../../services/post.service";
+import {
+  FacebookIcon,
+  FacebookShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+} from "react-share";
+import { userSelector } from "../../selectors/user.selector";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 const PostCard = ({ post }) => {
+  const history = useHistory()
+  const user = useSelector(userSelector);
+
+  // state
   const [commentBoxVisible, setCommentBoxVisible] = useState(false);
   const [hasLiked, setHasLiked] = useState(post.hasLiked);
   const [loading, setLoading] = useState(false);
 
+  // handlers
   const handleLike = () => {
     setLoading(true);
     setHasLiked((prev) => !prev);
     PostService.likePost({
-      body: { postId: post.postId, empId: post.empId },
+      body: { postId: post.postId, empId: user.entity.empId },
     }).then(
       (res) => {
         setHasLiked(res.data.hasLiked);
@@ -32,10 +46,40 @@ const PostCard = ({ post }) => {
     );
     setLoading(false);
   };
+
+  const redirectOnClick = () => {
+    console.log("clicked");
+    if (post.empId === user.entity.empId) {
+      history.push('/profile')
+    }
+    else if (post.empId !== user.entity.empId) {
+      history.push(`/${post.empId}`)
+    }
+  }
+  // component
+  const content = (
+    <div className="share-popover">
+      <TwitterShareButton
+        url={"application/url"}
+        title={post.content}
+        hashtags={post.tags}
+      >
+        <TwitterIcon size={32} round={true} />
+      </TwitterShareButton>
+      <FacebookShareButton
+        url={"application/url"}
+        quote={post.content}
+        hashtag={post.tags.join("#")}
+      >
+        <FacebookIcon size={32} round={true} />
+      </FacebookShareButton>
+    </div>
+  );
+
   return (
     <>
       <div className="post-card">
-        <div className="post-header">
+        <div className="post-header" onClick={redirectOnClick}>
           <img
             className="author-img"
             src={
@@ -51,7 +95,7 @@ const PostCard = ({ post }) => {
               {post.designation || "Software Developer"}
             </div>
             <div className="post-timestamp">
-              {new Date(post.createdAt + "Z").toLocaleString([], {
+              {new Date(post.createdAt + "Z").toLocaleString(["en-IN"], {
                 day: "numeric",
                 month: "numeric",
                 year: "numeric",
@@ -103,8 +147,15 @@ const PostCard = ({ post }) => {
               <span className="btn-text">Comment</span>
             </div>
             <div className="btn">
-              <ShareAltOutlined />
-              <span className="btn-text">Share</span>
+              <Popover
+                content={content}
+                title="Share"
+                overlayStyle={{ width: "120px" }}
+                trigger="click"
+              >
+                <ShareAltOutlined />
+                <span className="btn-text">Share</span>
+              </Popover>
             </div>
           </div>
         </div>
